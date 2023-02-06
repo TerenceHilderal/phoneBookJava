87ba14085af03e1eb3fcee2b2b00746f9d7e20bc
TerenceHilderal <terencehilderal@gmail.com>
2023-02-06 11:12:48 +0100

    refacto code

--- src/main/java/com/phoneBookJava/phonebook/PhoneBook.java
+++ src/main/java/com/phoneBookJava/phonebook/PhoneBook.java
@@ -1,11 +1,30 @@
 package com.phoneBookJava.phonebook;
 
+import java.util.Scanner;
+
 public class PhoneBook {
 
+	public static Scanner sc = null;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello world my friends ");
+
+		sc = new Scanner(System.in);
+
+		String userLastName = getUserInput("Entrez un nom de famille: ");
+		String userFirstName = getUserInput("Entrez un prénom: ");
+		String userPhoneNumber = getUserInput("Entrez un numéro de téléphone: ");
+
+		Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
+
+		System.out.println(newContact.toString());
+
+		sc.close();
 
 	}
 
+	public static String getUserInput(String userRequest) {
+
+		System.out.println(userRequest);
+		return sc.nextLine();
+	}
 }
